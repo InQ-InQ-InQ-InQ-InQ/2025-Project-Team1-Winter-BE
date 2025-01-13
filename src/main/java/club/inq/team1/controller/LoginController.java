ace5ad3b19602fb8445b76d96996a968cb4a0051
whrjsgml0000 <whrjsgml4515@naver.com>
2025-01-14 01:13:40 +0900

    feat : 회원가입 후 로그인 기능 작동 확인

---
+++ src/main/java/club/inq/team1/controller/LoginController.java
@@ -0,0 +1,12 @@
+package club.inq.team1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String loginP(){
+        return "login.html";
+    }
+}
--- src/main/java/club/inq/team1/service/impl/UserServiceImpl.java
+++ src/main/java/club/inq/team1/service/impl/UserServiceImpl.java
@@ -7,15 +7,18 @@ import club.inq.team1.service.UserService;
 import jakarta.transaction.Transactional;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -24,7 +27,7 @@ public class UserServiceImpl implements UserService {
         User user = new User();
 
         user.setUsername(userJoinDTO.getUsername());
-        user.setPassword(userJoinDTO.getPassword());
+        user.setPassword(passwordEncoder.encode(userJoinDTO.getPassword()));
 
         User saved = userRepository.save(user);
 
