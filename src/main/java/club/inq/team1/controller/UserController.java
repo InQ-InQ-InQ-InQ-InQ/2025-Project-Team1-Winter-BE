81ddfeb441aa115063690849c51827ec014154b9
whrjsgml0000 <whrjsgml4515@naver.com>
2025-01-14 17:08:17 +0900

    feat : 아이디 중복 확인 기능 추가

--- src/main/java/club/inq/team1/controller/UserController.java
+++ src/main/java/club/inq/team1/controller/UserController.java
@@ -11,9 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -30,12 +32,10 @@ public class UserController {
     }
 
     @PostMapping("/join")
-    @Operation(summary = "회원가입",
-            responses = {
+    @Operation(summary = "회원가입", responses = {
                     @ApiResponse(responseCode = "201", description = "아이디 생성 성공"),
                     @ApiResponse(responseCode = "401", description = "동일한 아이디를 가진 유저가 존재")
-            }
-    )
+    })
     public ResponseEntity<User> join(@RequestBody @Valid UserJoinDTO userJoinDTO) {
         if (userDetailsService.loadUserByUsername(userJoinDTO.getUsername()) != null) {
             // 이미 같은 아이디를 가진 유저가 존재.
@@ -45,5 +45,13 @@ public class UserController {
         User user = userService.acceptUser(userJoinDTO);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @GetMapping("/username")
+    @Operation(summary = "중복된 아이디인지 확인", description = "중복된 아이디라면 true, 아니라면 false", responses = {
+            @ApiResponse(responseCode = "200", description = "아이디 중복 조회 성공")
+    })
+    public ResponseEntity<Boolean> existsUsername(@RequestParam(value = "q") String username){
+        return ResponseEntity.status(200).body(userDetailsService.loadUserByUsername(username) != null);
     }
 }
