8d71487f6465edd11715a31d82f68d6ff1775520
EverJun2 <deverjun2@gmail.com>
2025-01-15 16:38:54 +0900

    feat : Follow entity 작성

---
+++ src/main/java/club/inq/team1/entity/Follow.java
@@ -0,0 +1,36 @@
+package club.inq.team1.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "follow")
+public class Follow {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "follow_id")
+    private Long followId;
+
+    @ManyToOne
+    @JoinColumn(name = "follower_id", nullable = false)
+    private User followerId;
+
+    @ManyToOne
+    @JoinColumn(name = "followee_id", nullable = false)
+    private User followeeId;
+
+    @Column(name = "alarm" , columnDefinition = "DEFAULT 0", nullable = false)
+    private Boolean alarm;
+}
