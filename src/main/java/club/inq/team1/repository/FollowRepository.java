5ecc4f30977b4411d064afcd6645ab1f912c8acf
EverJun2 <deverjun2@gmail.com>
2025-01-16 01:52:11 +0900

    feat : FollowRepository 생성

---
+++ src/main/java/club/inq/team1/repository/FollowRepository.java
@@ -0,0 +1,17 @@
+package club.inq.team1.repository;
+
+import club.inq.team1.entity.Follow;
+import club.inq.team1.entity.User;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FollowRepository extends JpaRepository<Follow, Long> {
+
+    List<Long> findFolloweesByFollowerId(User currentUser);
+    List<Long> findFollowersByFolloweeId(User currentUser);
+    Optional<Follow> findByFollowerIdAndFolloweeId(User opponentUser, User currentUser);
+
+    // 팔로우 관계 삭제 (팔로우하는 사람(followerId)과 팔로우되는 사람(followeeId) 기준)
+    void delete(Follow follow);  // JpaRepository에 있는 기본 delete 메소드 활용
+}
