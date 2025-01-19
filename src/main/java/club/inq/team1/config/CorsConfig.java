4369c6e963d2c15960cf5c9af10c3529f0e64797
whrjsgml0000 <whrjsgml4515@naver.com>
2025-01-19 15:37:03 +0900

    feat : EnableWebMvc 어노테이션 추가

--- src/main/java/club/inq/team1/config/CorsConfig.java
+++ src/main/java/club/inq/team1/config/CorsConfig.java
@@ -2,9 +2,12 @@ package club.inq.team1.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -16,4 +19,12 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowCredentials(true)
                 .maxAge(3600);
     }
+
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler()
+//                .addResourceLocations()
+//                .setCacheControl()
+//                .resourceChain()
+//    }
 }
