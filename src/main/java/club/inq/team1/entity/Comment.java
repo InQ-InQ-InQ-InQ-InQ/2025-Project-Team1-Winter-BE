10f2780e2a6f6e5feb553646c38ec5e17a24e564
Studying_Jin <kimjin5605@naver.com>
2025-01-20 00:27:41 +0900

    Modify files and add new features

---
+++ src/main/java/club/inq/team1/entity/Comment.java
@@ -0,0 +1,32 @@
+package club.inq.team1.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "comment")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
