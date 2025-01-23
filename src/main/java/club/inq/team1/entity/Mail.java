6331933c7986bcd22486b97f8cfd031e088d8c83
whrjsgml0000 <whrjsgml4515@naver.com>
2025-01-23 11:56:02 +0900

    feat : mail 엔티티 생성, User 엔티티와 연결

---
+++ src/main/java/club/inq/team1/entity/Mail.java
@@ -0,0 +1,36 @@
+package club.inq.team1.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "mail")
+@JsonIgnoreProperties(value = {"user","post"})
+public class Mail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "mail_id")
+    private Long mailId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+//    todo
+//    @ManyToOne
+//    @JoinColumn(name = "post_id")
+}
