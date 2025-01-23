8b2b621811817bfe777c1461e82891a2b2fe2636
whrjsgml0000 <whrjsgml4515@naver.com>
2025-01-23 16:56:11 +0900

    feat : 엔티티 수정
    + 알람이 온 시간에 대한 필드 추가

--- src/main/java/club/inq/team1/entity/Mail.java
+++ src/main/java/club/inq/team1/entity/Mail.java
@@ -3,17 +3,24 @@ package club.inq.team1.entity;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @NoArgsConstructor
@@ -22,12 +29,16 @@ import org.hibernate.annotations.ColumnDefault;
 @Setter
 @Table(name = "mail")
 @JsonIgnoreProperties(value = {"user","post"})
+@EntityListeners(AuditingEntityListener.class)
 public class Mail {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "mail_id")
     private Long mailId;
 
+    /**
+     * 알람 수신 여부. 기본 값은 false
+     */
     @Column(name = "saw", nullable = false)
     @ColumnDefault("false")
     private Boolean saw;
@@ -38,6 +49,13 @@ public class Mail {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    /**
+     * 알람이 온 시간
+     */
+    @Column(name = "createdAt")
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    private LocalDateTime createdAt;
 //    todo
 //    @ManyToOne
 //    @JoinColumn(name = "post_id")
