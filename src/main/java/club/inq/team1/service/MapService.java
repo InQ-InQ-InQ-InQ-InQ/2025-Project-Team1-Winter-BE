88dcc9368d367c8d00721017d60691669408c6a3
EverJun2 <deverjun2@gmail.com>
2025-02-08 01:13:36 +0900

    feat : MapService 인터페이스 작성

--- src/main/java/club/inq/team1/service/MapService.java
+++ src/main/java/club/inq/team1/service/MapService.java
@@ -1,5 +1,19 @@
 package club.inq.team1.service;
 
+import club.inq.team1.dto.request.map.AddressRequestDTO;
+import club.inq.team1.dto.response.map.GeocodeResponseDTO;
+
 public interface MapService {
 
+    //위경도 -> 주소 변환
+    String convertAddress(AddressRequestDTO addressRequestDTO);
+
+    //주소 -> 위경도 변환
+    GeocodeResponseDTO convertGeocode(String address);
+
+    //지역명 변환
+    String convertRegion(String address);
+
+    //해당 주소가 있는 주소인지 확인
+    boolean searchAddress(String address);
 }
