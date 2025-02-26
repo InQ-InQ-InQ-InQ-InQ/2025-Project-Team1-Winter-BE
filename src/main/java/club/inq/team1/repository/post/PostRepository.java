d94ea6c86d6997f8856cc3d7c8e85495fa91bff3
EverJun2 <deverjun2@gmail.com>
2025-02-26 11:15:20 +0900

    feat : 조회수 증가 메소드 선언 (동시성 문제 예방을 위해 직접 쿼리 사용?)

--- src/main/java/club/inq/team1/repository/post/PostRepository.java
+++ src/main/java/club/inq/team1/repository/post/PostRepository.java
@@ -4,11 +4,20 @@ import club.inq.team1.entity.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
     Page<Post> findByTitleContainingOrContentContaining(String titleKeyword, String contentKeyword, Pageable pageable);
     Page<Post> findByTagsContaining(String tag, Pageable pageable);
     Page<Post> findByRegionContaining(String region, Pageable pageable);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Post post SET post.hit = post.hit + 1 WHERE post.postId = :postId")
+    void increaseHit(@Param("postId") Long postId);
 }
