3d5c70939aa95efac9105bcce278ad1066bf3f12
EverJun2 <deverjun2@gmail.com>
2025-02-27 18:35:09 +0900

    feat : 범위 내 좌표 저장된 게시글 검색 기능 repository 에 추가

--- src/main/java/club/inq/team1/repository/post/PostRepository.java
+++ src/main/java/club/inq/team1/repository/post/PostRepository.java
@@ -1,6 +1,7 @@
 package club.inq.team1.repository.post;
 
 import club.inq.team1.entity.Post;
+import java.math.BigDecimal;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +12,5 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     Page<Post> findByTitleContainingOrContentContaining(String titleKeyword, String contentKeyword, Pageable pageable);
     Page<Post> findByTagsContaining(String tag, Pageable pageable);
     Page<Post> findByRegionContaining(String region, Pageable pageable);
+    Page<Post> findByLatitudeBetweenAndLongitudeBetween(BigDecimal leftX, BigDecimal rightX, BigDecimal leftY, BigDecimal rightY, Pageable pageable);
 }
